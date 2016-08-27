51d297f779e2c9979ca0a101374076283fa64d3c
MaciejSzpakowski <systemzajety@yahoo.com>
2016-08-27 11:14:32 -0500

    Some commit

--- HeaderGenerator/Program.cs
+++ HeaderGenerator/Program.cs
@@ -11,7 +11,7 @@ namespace HeaderGenerator
     {
         static void Main(string[] args)
         {
-            string dir = "C:/Users/Szpak/Documents/Visual Studio 2015/Projects/cppConsole/cppConsole";
+            string dir = "C:/Users/Szpak/Documents/Visual Studio 2015/Projects/HeaderGenerator/Test";
             List<string> files = (new[] { "Source1.cpp", "Source2.cpp", "Source3.cpp", "Source4.cpp"}).ToList();
             string h = Path.Combine(dir, "Header.h");
             string cpp = Path.Combine(dir, "Source.cpp");
